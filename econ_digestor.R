729acfea67445ff03e6fe47fb09235a5173acd79
mert0248 <simon.procter@gmail.com>
2020-11-16 14:48:57 +0000

    Updated to calculated outputs. Returns: total costs and total dalys for both the intervention scenario and the baseline comparator; incremental costs and incremental dalys; ICERs N.B. ICERs are expressed in terms of DALYs averted

--- econ_digestor.R
+++ econ_digestor.R
@@ -95,12 +95,12 @@ econ_digestor <- function(epi_scen.dt, epi_base.dt, dalys.dt, econ_pars){
             # TODO: update with prevalent cases or alternative assumption about
             # treatment duration at home
             (cases - (non_icu_severe_i + non_icu_critical_i)) * 
-                3.5 * (cost_hh_treat_home_med_per_day + 
+                7 * (cost_hh_treat_home_med_per_day + 
                      cost_hh_treat_home_non_med_per_day) +
             # household: individual and caregiver lost income
             # TODO: update with prevalent cases or alternative assumption about
             # days of lost income per case
-            cases * 3.5 * (cost_hh_individual_income_per_day +
+            cases * 7 * (cost_hh_individual_income_per_day +
                                cost_hh_caregiver_income_per_day)
         )])
         return(scen.dt)
@@ -127,14 +127,156 @@ econ_digestor <- function(epi_scen.dt, epi_base.dt, dalys.dt, econ_pars){
             death_o * daly.dt[age_cat == age_cat, dalys]
     ])
     
-    with(econ_pars,epi_scen.dt[,
+    with(econ_pars,epi_base.dt[,
         dalys := (1/(1 + disc.rate.daly)^(anni_year-1)) *
             death_o * daly.dt[age_cat == age_cat, dalys]
     ])
 
-    # CALCULATION OF OUTPUTS HERE
-
-
+    # melt costs and dalys
+    costs.scen <- melt(
+        epi_scen.dt,
+        id.vars = c("scenarioId","sampleId","age","anni_year"),
+        measure.vars = c("costs")
+    )
     
+    dalys.scen <- melt(
+        epi_scen.dt,
+        id.vars = c("scenarioId","sampleId","age","anni_year"),
+        measure.vars = c("dalys")
+    )
+    
+    costs.base <- melt(
+        epi_base.dt,
+        id.vars = c("scenarioId","sampleId","age","anni_year"),
+        measure.vars = c("costs")
+    )
+    
+    dalys.base <- melt(
+        epi_base.dt,
+        id.vars = c("scenarioId","sampleId","age","anni_year"),
+        measure.vars = c("dalys")
+    )
+
+    # cumulative values over time
+    accumulate.dt <- function(dt, tvar=expression(anni_year)) {
+        dt[order(eval(tvar)), cvalue := cumsum(value), by=setdiff(names(dt),c(tvar, "value","cvalue"))]
+    }
+    
+    accumulate.dt(costs.scen)
+    accumulate.dt(costs.base)
+    
+    accumulate.dt(dalys.scen)
+    accumulate.dt(dalys.base)
+
+    # sum over age groups
+    allage.dt <- function(dt) {
+        dt[, .(
+            value = sum(value), cvalue = sum(value), age = "all"
+        ), by=setdiff(names(dt), c("age","value","cvalue"))]
+    }
+    
+    sum.costs.scen <- allage.dt(costs.scen)
+    sum.costs.base <- allage.dt(costs.base)
+    
+    sum.dalys.scen <- allage.dt(dalys.scen)
+    sum.dalys.base <- allage.dt(dalys.base)
+    
+    # incremental costs and dalys
+    
+    baseline.costs <- sum.costs.base # can't use sum.costs.base directly not sure why
+    
+    incr.costs.dt <- sum.costs.scen[
+        baseline.costs, on=.(sampleId, anni_year, variable, age)
+    ][, .(
+        scenarioId, sampleId, anni_year, variable, age,
+        incr = i.value - value, cum_incr = i.cvalue - cvalue
+    )]
+    
+    baseline.dalys <- sum.dalys.base # can't use sum.dalys.base directly not sure why
+    
+    incr.dalys.dt <- sum.dalys.scen[
+        baseline.dalys, on=.(sampleId, anni_year, variable, age)
+    ][, .(
+        scenarioId, sampleId, anni_year, variable, age,
+        incr = i.value - value, cum_incr = i.cvalue - cvalue
+    )]
+    
+    # ICER
+    icer.dt <- incr.costs.dt[variable == "costs"][
+        incr.dalys.dt[variable == "dalys"], 
+        on = .(scenarioId, sampleId, anni_year, age),
+        allow.cartesian = TRUE
+    ]
+    icer.dt[, icer := -cum_incr / i.cum_incr ] # -ve as want dalys averted
+    
+    # Quantiles
+    qtile <- function(
+        v, ps = c(lo95=0.025, lo50=0.25, md=0.5, hi50=0.75, hi95=0.975),
+        withMean = c("mn", NA),
+        fmt = "value.%s"
+    ) {
+        qs <- quantile(v, probs = ps)
+        names(qs) <- sprintf(fmt, names(ps))
+        if (!is.na(withMean[1])) {
+            mn <- mean(v)
+            names(mn) <- sprintf(fmt, withMean[1])
+            qs <- c(qs, mn)
+        }
+        as.list(qs)
+    }
+    
+    q.costs.scen.dt <- sum.costs.scen[,{
+        qv <- qtile(value)
+        qc <- qtile(cvalue, fmt = "cvalue.%s")
+        c(qv, qc)
+    },by=setdiff(names(sum.costs.scen),c("sampleId","value","cvalue"))]
+    
+    q.costs.base.dt <- sum.costs.base[,{
+        qv <- qtile(value)
+        qc <- qtile(cvalue, fmt = "cvalue.%s")
+        c(qv, qc)
+    },by=setdiff(names(sum.costs.base),c("sampleId","value","cvalue"))]
+    
+    qincr.costs.dt <- incr.costs.dt[,{
+        qv <- qtile(incr, fmt = "incr.%s")
+        qc <- qtile(cum_incr, fmt = "cum_incr.%s")
+        c(qv, qc)
+    },by=setdiff(names(incr.costs.dt),c("sampleId","incr","cum_incr"))]
+    
+    
+    q.dalys.scen.dt <- sum.dalys.scen[,{
+        qv <- qtile(value)
+        qc <- qtile(cvalue, fmt = "cvalue.%s")
+        c(qv, qc)
+    },by=setdiff(names(sum.dalys.scen),c("sampleId","value","cvalue"))]
+    
+    q.dalys.base.dt <- sum.dalys.base[,{
+        qv <- qtile(value)
+        qc <- qtile(cvalue, fmt = "cvalue.%s")
+        c(qv, qc)
+    },by=setdiff(names(sum.dalys.base),c("sampleId","value","cvalue"))]
+    
+    qincr.dalys.dt <- incr.dalys.dt[,{
+        qv <- qtile(incr, fmt = "incr.%s")
+        qc <- qtile(cum_incr, fmt = "cum_incr.%s")
+        c(qv, qc)
+    },by=setdiff(names(incr.dalys.dt),c("sampleId","incr","cum_incr"))]
+    
+    q.icer.dt <- icer.dt[,{
+        qtile(icer)
+    },by=.(scenarioId, anni_year)]
+    
+    # Return results
+
+    return(list(
+        q.costs.scen.dt = q.costs.scent.dt, # costs under intervention
+        q.costs.base.dt = q.costs.base.dt,  # costs under base case
+        qincr.costs.dt  = qincr.costs.dt,   # incremental costs
+        q.dalys.scen.dt = q.dalys.scent.dt, # dalys under intervention
+        q.dalys.base.dt = q.dalys.base.dt,  # dalys under base case
+        qincr.dalys.dt  = qincr.dalyss.dt,  # incremental dalys
+        q.icer.dt       = q.icer.dt         # icer cost per daly AVERTED
+        
+    ))
 }
 
